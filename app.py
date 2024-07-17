68b3a3ce9fb01ecdbab92fea83768375166e1a34
khyati-arora <Khyati.a@consultadd.com>
2024-07-17 16:01:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,113 +1,122 @@
 from flask import Flask, request, jsonify
-import json
-from flask_mysqldb import MySQL
-import hashlib
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_serializer import SerializerMixin
+
 app = Flask(__name__)
+username = 'root'
+password = 'Khyati123'
+host = 'localhost'
+port = 3306
+DB_NAME = 'products'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Nksk12@3'
-app.config['MYSQL_DB'] = 'products'
+engine = create_engine(f"mysql://{username}:{password}@{host}:{port}/{DB_NAME}")
+Session = sessionmaker(bind=engine)
+session = Session()
 
-mysql = MySQL(app)
+Base = declarative_base()
 
+class User(Base, SerializerMixin):
+    __tablename__ = "Users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String)
 
+# Create the table in the database
+Base.metadata.create_all(engine)
 
-def create_table():
-     cur = mysql.connection.cursor()
-     cur.execute("CREATE TABLE IF NOT EXISTS Users (id INT AUTO_INCREMENT PRIMARY KEY ,username VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(50) NOT NULL, role VARCHAR(50))")
-     mysql.connection.commit()
-     cur.close()
-
-with app.app_context():
-    create_table()    
-
-def get_user_data(id): 
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT id,username,role FROM Users WHERE id = %s''', (id,))
-    data = cur.fetchall()
-    cur.close()
-    if not data:
-        return "not found"
-    return jsonify(data), 200
-  
-@app.route('/')
-def getResult():
-   try:
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT id,username,role FROM Users")
-    data = cur.fetchall()
-    print(data)
-    cur.close()
-    return jsonify(data)
-   except Exception as e:
-        return jsonify({'error': str(e)}), 500
-   
-
-
-@app.route('/data/<int:id>')
-def get_user(id):
-  try:  
-    return get_user_data(id)
-  except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-@app.route('/add',methods = ['POST'])
-def addUser():
+@app.route('/add', methods=['POST'])
+def add_user():
     try:
         data = request.json
-        if not data or not all(key in data for key in ( 'username', 'password', 'role')):
-                return jsonify({'error': 'Missing data'}), 400
-        cur = mysql.connection.cursor()
-        username = data['username']
-        hashed_password = hashlib.md5(data['password'].encode('utf-8')).hexdigest()
-        role = data['role']
-        data['password'] = hashed_password
-        cur.execute('''INSERT INTO Users (username,password,role) VALUES (%s, %s, %s)''', (username,hashed_password,role))
-        mysql.connection.commit()
-        cur.close()
+        if not data or not all(key in data for key in ('username', 'password', 'role')):
+            return jsonify({'error': 'Missing data'}), 400
+        
+        new_user = User(username=data['username'], password=data['password'], role=data['role'])
+        session.add(new_user)
+        session.commit()
+
         return jsonify(data), 201
     
     except Exception as e:
+        session.rollback()  # Rollback the session in case of an error
         return jsonify({'error': str(e)}), 500
 
-
-@app.route('/update/<int:id>', methods = ['PUT'] )
-def updateUser(id):
+@app.route('/')
+def get_users():
     try:
+        users = session.query(User).all()
+        users_dict = []
+        for user in users:
+            user_data = {
+                'id': user.id,
+                'username': user.username,
+                'role': user.role
+            }
+            users_dict.append(user_data)
+        return jsonify(users=users_dict)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/data/<int:id>')
+def get_user_with_id(id):
+    try:
+        user = session.query(User).get(id)
+        if user:
+            user_data = {
+                'id': user.id,
+                'username': user.username,
+                'role': user.role
+            }
+            return jsonify(user_data)
+        else:
+            return jsonify({'error': 'User not found'}), 404
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/update/<int:id>', methods=['PUT'])
+def update_user(id):
+    try:
+        user = session.query(User).get(id)
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
+        
         data = request.json
-        if not data or not all(key in data for key in ('username', 'role')):
-            return jsonify({'error': 'Missing data'}), 400
-        
-        value = get_user_data(id)
-        
-        if value == "not found" : 
-            return jsonify({'error': 'Missing user'}), 404
-        
-        cur = mysql.connection.cursor()
+        user.username = data['username']
+        user.role = data['role']
+        session.commit()
 
-        username = data['username']
-        role = data['role']
-        cur.execute('''UPDATE Users SET username = %s, role = %s WHERE id = %s''', (username,role,id))
-        mysql.connection.commit()
-        cur.close()
-        return jsonify({'message': 'Data updated successfully'})
+        return jsonify({'msg': 'User update successful'}), 200
+    
     except Exception as e:
+        session.rollback()
         return jsonify({'error': str(e)}), 500
 
-@app.route('/delete/<int:id>',methods=["DELETE"])
-def deleteUser(id):
-    
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_user(id):
     try:
-        cur = mysql.connection.cursor()
-        cur.execute('''DELETE FROM Users WHERE id = %s''', (id,))
-        mysql.connection.commit()
-        cur.close()
-        return jsonify({'message': 'Data deleted successfully'})
+        user = session.query(User).get(id)
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
+        
+        session.delete(user)
+        session.commit()
+
+        return jsonify({'msg': 'User deleted successfully'}), 200
     
     except Exception as e:
+        session.rollback()
         return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
     
 
 
